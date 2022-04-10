eb3b424b0d15f89a7f255b18ab8af3f6e9c64b74
Tom <blak1002@gmail.com>
2022-04-10 09:16:09 +0900

    [feat] commit mainActivity UI

---
+++ android/BookChat/app/src/main/java/com/example/bookchat/repository/TestRepository.kt
@@ -0,0 +1,12 @@
+package com.example.bookchat.repository
+
+//HTTP 통신 혹은 DB작업 따로 분리
+class TestRepository {
+
+    fun getUsetInfo() : ArrayList<String> {
+        //레트로핏 함수 호출
+//        retrofit.getUserInfo()
+        val list = ArrayList<String>()
+        return list
+    }
+}
---
+++ android/BookChat/app/src/main/java/com/example/bookchat/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.bookchat.utils
+
+object Constansts {
+    const val TAG: String = "로그"
+}
