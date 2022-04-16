f7e349a2ec1a97573b38628d7a6f3011c5ea59a1
geneaky <kaktus418@gmail.com>
2022-04-16 12:12:17 +0900

    [fix] seperate url for deep link

--- back/src/main/java/toy/bookchat/bookchat/security/handler/CustomAuthenticationSuccessHandler.java
+++ back/src/main/java/toy/bookchat/bookchat/security/handler/CustomAuthenticationSuccessHandler.java
@@ -1,5 +1,13 @@
 package toy.bookchat.bookchat.security.handler;
 
+import static toy.bookchat.bookchat.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -9,30 +17,22 @@ import org.springframework.web.util.UriComponentsBuilder;
 import toy.bookchat.bookchat.security.CookieUtils;
 import toy.bookchat.bookchat.security.jwt.JwtTokenProvider;
 import toy.bookchat.bookchat.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
-import toy.bookchat.bookchat.security.oauth.OAuth2Provider;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Optional;
-
-import static toy.bookchat.bookchat.security.oauth.HttpCookieOAuth2AuthorizationRequestRepository.*;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    public static final String AUTH_PATH = "auth";
     private final JwtTokenProvider jwtTokenProvider;
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
         String targetUrl = determineTargetUrl(request, response, authentication);
 
-        if(response.isCommitted()) {
+        if (response.isCommitted()) {
             log.debug("Response has already been committed. Unable to redirect to " + targetUrl);
             return;
         }
@@ -41,22 +41,26 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
-                .map(Cookie::getValue);
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) {
+        Optional<String> redirectUri = CookieUtils.getCookie(request,
+                REDIRECT_URI_PARAM_COOKIE_NAME)
+            .map(Cookie::getValue);
 
         String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
         String jwtToken = jwtTokenProvider.createToken(authentication);
 
         return UriComponentsBuilder.fromUriString(targetUrl)
-                .queryParam("token", jwtToken)
-                .build().toUriString();
+            .path(AUTH_PATH)
+            .queryParam("token", jwtToken)
+            .build().toUriString();
     }
 
-    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+    protected void clearAuthenticationAttributes(HttpServletRequest request,
+        HttpServletResponse response) {
         super.clearAuthenticationAttributes(request);
         httpCookieOAuth2AuthorizationRequestRepository
-                .removeAuthorizationRequestCookies(request, response);
+            .removeAuthorizationRequestCookies(request, response);
     }
 }
 
