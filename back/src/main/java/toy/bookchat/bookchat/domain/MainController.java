e5367f132fc0282fa0ae99ace82724b09b31687c
geneaky <kaktus418@gmail.com>
2022-05-11 23:57:31 +0900

    [fix] add button for android api test

--- back/src/main/java/toy/bookchat/bookchat/domain/MainController.java
+++ back/src/main/java/toy/bookchat/bookchat/domain/MainController.java
@@ -38,10 +38,24 @@ public class MainController {
             + "</body>\n" + "</html>";
     }
 
-    @GetMapping("/app")
+    @GetMapping(value = "/app", produces = MediaType.TEXT_HTML_VALUE)
     public String comebackToApp(@RequestParam String token, HttpServletResponse response) {
-        response.setHeader("token", token);
-        return "have a good time";
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        return "<html>\n" + "<header><title>BookChat</title>"
+            + "</header>\n" +
+            "<body>\n" + "<p> have a good time! </p>\n"
+            + "<a href=\"https://bookchat.link/app?token=" + token
+            + "\" style=\"font: bold 11px Arial;\n"
+            + "  text-decoration: none;\n"
+            + "  background-color: #EEEEEE;\n"
+            + "  color: #333333;\n"
+            + "  padding: 2px 6px 2px 6px;\n"
+            + "  border-top: 1px solid #CCCCCC;\n"
+            + "  border-right: 1px solid #333333;\n"
+            + "  border-bottom: 1px solid #333333;\n"
+            + "  border-left: 1px solid #CCCCCC; \"> back to BookChat </a>"
+            + "</body>\n" + "</html>";
     }
 
 }
