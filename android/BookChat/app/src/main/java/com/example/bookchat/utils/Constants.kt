c9e299b06d8786858c510e1b42814a7c5b526443
Tom <blak1002@gmail.com>
2022-07-08 06:45:30 +0900

    [fix] Apply .gitignore

--- android/BookChat/app/src/main/java/com/example/bookchat/utils/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bookchat.utils
-
-object Constants {
-    const val TAG: String = "로그"
-    const val KAKAO_LOGIN: String = "https://bookchat.link/oauth2/authorize/kakao"
-    const val GOOGLE_LOGIN: String = "https://bookchat.link/oauth2/authorize/google"
-    const val DOMAIN = "https://bookchat.link/"
-}
