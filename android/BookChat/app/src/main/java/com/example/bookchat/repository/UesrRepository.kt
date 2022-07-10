530f65af2d2ca9e138d0dc2d2a30c17f3384dc20
Tom <blak1002@gmail.com>
2022-07-10 10:11:00 +0900

    [refector] UserRepository modify Anonymous Class

--- android/BookChat/app/src/main/java/com/example/bookchat/repository/UesrRepository.kt
+++ android/BookChat/app/src/main/java/com/example/bookchat/repository/UesrRepository.kt
@@ -16,30 +16,28 @@ class UesrRepository{
             return
         }
         val call = App.instance.apiInterface.getUserProfile()
-        call.enqueue(object : Callback<User> {
-            override fun onResponse(
-                call: Call<User>,
-                response: Response<User>
-            ) {
-                if(response.isSuccessful){
-                    Log.d(TAG, "UesrRepository: onResponse() - Success(통신 성공) response.body() : ${response.body()}")
-                    val user = response.body()!!
-                    callback(user)
-                    return
-                }
-                Log.d(TAG, "UesrRepository: onResponse() - Fail(통신 실패) 응답 코드: ${response.code()}")
-                Toast.makeText(App.instance.applicationContext,"서버와의 연결을 실패했습니다.\n응답 코드: ${response.code()}", Toast.LENGTH_SHORT).show()
-            }
-            override fun onFailure(
-                call: Call<User>,
-                t: Throwable) {
-                //인터넷 끊김 , 예외 발생 등 시스템적 이유로 통신 실패
-                Log.d(TAG, "UesrRepository: onFailure()- Throwable : ${t} ")
-            }
-        })
+        call.enqueue(CallbackAction(callback))
     }
     private fun isNetworkConnected() :Boolean{
         return App.instance.networkManager.checkNetworkState()
     }
+
+    inner class CallbackAction(val callback : (User) -> Unit) : Callback<User> {
+        override fun onResponse(call: Call<User>, response: Response<User>) {
+            if(response.isSuccessful){
+                Log.d(TAG, "UesrRepository: onResponse() - Success(통신 성공) response.body() : ${response.body()}")
+                val user = response.body()!!
+                callback(user)
+                return
+            }
+            Log.d(TAG, "UesrRepository: onResponse() - Fail(통신 실패) 응답 코드: ${response.code()}")
+            Toast.makeText(App.instance.applicationContext,"서버와의 연결을 실패했습니다.\n응답 코드: ${response.code()}", Toast.LENGTH_SHORT).show()
+        }
+        override fun onFailure(call: Call<User>, t: Throwable) {
+            //인터넷 끊김 , 예외 발생 등 시스템적 이유로 통신 실패
+            Log.d(TAG, "UesrRepository: onFailure()- Throwable : ${t} ")
+        }
+    }
+
 }
 
