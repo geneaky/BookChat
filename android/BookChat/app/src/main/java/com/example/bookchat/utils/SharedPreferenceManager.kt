8063f40373b5e699b8ec05f8fbc585f6fce82e1d
Tom <blak1002@gmail.com>
2022-07-10 20:59:04 +0900

    [feat] Encrypt user access tokens and store in Shared Preferences

--- android/BookChat/app/src/main/java/com/example/bookchat/utils/SharedPreferenceManager.kt
+++ android/BookChat/app/src/main/java/com/example/bookchat/utils/SharedPreferenceManager.kt
@@ -3,18 +3,25 @@ package com.example.bookchat.utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.example.bookchat.App
 import com.example.bookchat.utils.Constants.TAG
 
 object SharedPreferenceManager {
 
-    const val TOKEN_PREF = "TOKEN_PREF"
     const val TOKEN_PREF_KEY = "TOKEN_PREF_KEY"
-
     //SharedPreferences 인스턴스 생성
     //App.instance = 앱 컨텍스트 받아오기
-    private var tokenPref :SharedPreferences =
-        App.instance.getSharedPreferences(TOKEN_PREF, Context.MODE_PRIVATE)
+    val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+    val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+    val tokenPref = EncryptedSharedPreferences.create(
+        "encryptedShared",
+        masterKeyAlias,
+        App.instance,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
 
     //토큰 저장하기
     fun saveToken(token :String){
