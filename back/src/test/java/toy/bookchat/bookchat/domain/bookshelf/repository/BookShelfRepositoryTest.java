17429449262d0e7d1e8d19b1047cba6c55483c61
geneaky <kaktus418@gmail.com>
2022-07-13 12:57:59 +0900

    [test -ing] back up

--- back/src/test/java/toy/bookchat/bookchat/domain/bookshelf/repository/BookShelfRepositoryTest.java
+++ back/src/test/java/toy/bookchat/bookchat/domain/bookshelf/repository/BookShelfRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import toy.bookchat.bookchat.domain.book.Book;
 import toy.bookchat.bookchat.domain.book.repository.BookRepository;
 import toy.bookchat.bookchat.domain.bookshelf.BookShelf;
+import toy.bookchat.bookchat.domain.bookshelf.ReadingStatus;
+import toy.bookchat.bookchat.domain.bookshelf.Star;
 import toy.bookchat.bookchat.domain.user.User;
 import toy.bookchat.bookchat.domain.user.repository.UserRepository;
 
@@ -60,7 +62,24 @@ public class BookShelfRepositoryTest {
     public void 읽고있는_책을_조회() throws Exception {
         Book book = new Book("1234", "effective java", List.of("Joshua"), "insight",
             "bookCover@naver.com");
+        bookRepository.save(book);
 
+        User user = User.builder().build();
+        userRepository.save(user);
 
+        BookShelf bookShelf = BookShelf.builder()
+            .book(book)
+            .user(user)
+            .readingStatus(ReadingStatus.READING)
+            .star(Star.ZERO)
+            .singleLineAssessment(null)
+            .build();
+
+        book.setBookShelf(bookShelf);
+        user.setBookShelf(bookShelf);
+
+        bookShelfRepository.save(bookShelf);
+
+        bookShelfRepository.findSpecificReadingStateBookByUserId(ReadingStatus.READING, ,user)
     }
 }
