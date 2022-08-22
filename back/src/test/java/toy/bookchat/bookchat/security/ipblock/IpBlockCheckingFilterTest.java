2dba4cc4d78c75ed31318f3bf0bf707826e790e7
geneaky <kaktus418@gmail.com>
2022-08-23 00:00:46 +0900

    [test] change test ip block checking filter

--- back/src/test/java/toy/bookchat/bookchat/security/ipblock/IpBlockCheckingFilterTest.java
+++ back/src/test/java/toy/bookchat/bookchat/security/ipblock/IpBlockCheckingFilterTest.java
@@ -1,20 +1,19 @@
 package toy.bookchat.bookchat.security.ipblock;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import javax.servlet.FilterChain;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.PrintWriter;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import toy.bookchat.bookchat.security.ipblock.exception.BlockedIpException;
 
 @ExtendWith(MockitoExtension.class)
 class IpBlockCheckingFilterTest {
@@ -29,15 +28,13 @@ class IpBlockCheckingFilterTest {
     public void IP가_block일때_다음_필터를_타고_진행_실패() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
-        PrintWriter writer = mock(PrintWriter.class);
         FilterChain filterChain = mock(FilterChain.class);
 
         when(ipBlockManager.validateRequest(request)).thenReturn(false);
-        when(response.getWriter()).thenReturn(writer);
 
-        ipBlockCheckingFilter.doFilterInternal(request, response, filterChain);
-
-        verify(filterChain,never()).doFilter(request,response);
+        Assertions.assertThatThrownBy(() -> {
+            ipBlockCheckingFilter.doFilterInternal(request, response, filterChain);
+        }).isInstanceOf(BlockedIpException.class);
     }
 
     @Test
@@ -50,6 +47,6 @@ class IpBlockCheckingFilterTest {
 
         ipBlockCheckingFilter.doFilterInternal(request, response, filterChain);
 
-        verify(filterChain).doFilter(request,response);
+        verify(filterChain).doFilter(request, response);
     }
 }
