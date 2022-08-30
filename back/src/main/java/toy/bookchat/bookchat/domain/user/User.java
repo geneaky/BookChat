4a109b5c3fba558af8b542b6160de3484719406c
geneaky <kaktus418@gmail.com>
2022-08-30 16:40:00 +0900

    [chore] remove password field

--- back/src/main/java/toy/bookchat/bookchat/domain/user/User.java
+++ back/src/main/java/toy/bookchat/bookchat/domain/user/User.java
@@ -24,24 +24,25 @@ public class User {
     private String name;
     private String nickName;
     private String email;
-    private String password;
     private String profileImageUrl;
     private ROLE role;
     @Enumerated(EnumType.STRING)
     private OAuth2Provider provider;
     @OneToMany(mappedBy = "user")
     private List<BookShelf> bookShelves = new ArrayList<>();
+    @ElementCollection
+    private List<ReadingTaste> readingTastes = new ArrayList<>();
 
     @Builder
-    private User(String name, String email, String password, String profileImageUrl, ROLE role,
-        OAuth2Provider provider, String nickName) {
+    public User(String name, String email, String profileImageUrl, ROLE role,
+        OAuth2Provider provider, String nickName, List<ReadingTaste> readingTastes) {
         this.name = name;
         this.email = email;
-        this.password = password;
         this.profileImageUrl = profileImageUrl;
         this.role = role;
         this.provider = provider;
         this.nickName = nickName;
+        this.readingTastes = readingTastes;
     }
 
     public void setBookShelf(BookShelf bookShelf) {
