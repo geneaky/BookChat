6f29c7abf0e2d5f869c4115754fb537ad41cfd4d
geneaky <kaktus418@gmail.com>
2022-08-31 08:56:31 +0900

    [feat] aws s3 config

--- back/src/main/java/toy/bookchat/bookchat/config/aws/AWSConfig.java
+++ back/src/main/java/toy/bookchat/bookchat/config/aws/AWSConfig.java
@@ -1,9 +1,7 @@
 package toy.bookchat.bookchat.config.aws;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Builder;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +10,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AWSConfig {
 
+    private final S3Config s3Config;
 
+    public AWSConfig(S3Config s3Config) {
+        this.s3Config = s3Config;
+    }
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials()
-        return AmazonS3ClientBuilder
-                .standard()
-                .withRegion()
-                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(s3Config.getAccessKey(),
+            s3Config.getSecretKey());
+        return (AmazonS3Client) AmazonS3ClientBuilder
+            .standard()
+            .withRegion(s3Config.getRegion())
+            .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+            .build();
     }
 }
--- back/src/main/java/toy/bookchat/bookchat/config/aws/S3Config.java
+++ back/src/main/java/toy/bookchat/bookchat/config/aws/S3Config.java
@@ -4,8 +4,35 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("s3")
+@ConfigurationProperties("aws.s3")
 public class S3Config {
 
-    private String
+    private String accessKey;
+    private String secretKey;
+
+    private String region;
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
 }
