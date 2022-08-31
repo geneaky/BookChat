ec9ff5a4925c68bb9756f34e0af81d3772163edd
geneaky <kaktus418@gmail.com>
2022-08-31 13:48:23 +0900

    [refactor] refactor java property config to be immutable

--- back/src/main/java/toy/bookchat/bookchat/config/aws/S3Config.java
+++ back/src/main/java/toy/bookchat/bookchat/config/aws/S3Config.java
@@ -1,38 +1,22 @@
 package toy.bookchat.bookchat.config.aws;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
-@Configuration
-@ConfigurationProperties("aws.s3")
+@Getter
+@ConstructorBinding
+@ConfigurationProperties(prefix = "aws.s3")
 public class S3Config {
 
-    private String accessKey;
-    private String secretKey;
+    private final String accessKey;
+    private final String secretKey;
+    private final String region;
 
-    private String region;
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
+    public S3Config(String accessKey, String secretKey, String region) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
         this.region = region;
     }
-
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public String getSecretKey() {
-        return secretKey;
-    }
-
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
-    }
 }
+
