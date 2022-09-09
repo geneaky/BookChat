b55853d4d629fa7fc2ae82ecdf07ff962fe12002
geneaky <kaktus418@gmail.com>
2022-09-09 18:55:59 +0900

    [backup & test] make OpenIdTestUtils for RSA authentication

---
+++ back/src/test/java/toy/bookchat/bookchat/security/openid/OpenIdTestUtil.java
@@ -0,0 +1,37 @@
+package toy.bookchat.bookchat.security.openid;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class OpenIdTestUtil {
+
+    private BufferedReader privateKeyReader;
+    private BufferedReader publicKeyReader;
+
+    public OpenIdTestUtil(String privagteKey, String publicKey) throws FileNotFoundException {
+        this.privateKeyReader = new BufferedReader(new FileReader(privagteKey));
+        this.publicKeyReader = new BufferedReader(new FileReader(publicKey));
+    }
+
+    public String getPrivateKey(int lineSize) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        this.privateKeyReader.readLine();
+        for (int i = 0; i < lineSize - 2; i++) {
+            stringBuilder.append(this.privateKeyReader.readLine());
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public String getPublicKey(int lineSize) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        this.publicKeyReader.readLine();
+        for (int i = 0; i < lineSize - 2; i++) {
+            stringBuilder.append(this.publicKeyReader.readLine());
+        }
+
+        return stringBuilder.toString();
+    }
+}
