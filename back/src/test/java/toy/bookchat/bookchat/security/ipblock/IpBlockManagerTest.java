45103c9dbf0dd478f7c3407685d28983fd924a6a
geneaky <kaktus418@gmail.com>
2022-10-06 12:45:46 +0900

    [chore&revise] for android test

--- back/src/test/java/toy/bookchat/bookchat/security/ipblock/IpBlockManagerTest.java
+++ back/src/test/java/toy/bookchat/bookchat/security/ipblock/IpBlockManagerTest.java
@@ -1,22 +1,17 @@
 package toy.bookchat.bookchat.security.ipblock;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import javax.servlet.http.HttpServletRequest;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class IpBlockManagerTest {
@@ -29,7 +24,7 @@ class IpBlockManagerTest {
 
     @Test
     public void XFF가없을때_지정된_횟수보다_더_많이_유효하지않은_요청을_보낸경우_false() throws Exception {
-        AccessIp accessIp = new AccessIp("0.0.0.0",101L);
+        AccessIp accessIp = new AccessIp("0.0.0.0", 10001L);
         HttpServletRequest request = mock(HttpServletRequest.class);
 
         when(accessIpRepository.findById("0.0.0.0")).thenReturn(Optional.of(accessIp));
@@ -42,7 +37,7 @@ class IpBlockManagerTest {
 
     @Test
     public void XFF가있을때_지정된_횟수보다_적게_유효하지않은_요청을_보낸경우_true() throws Exception {
-        AccessIp accessIp = new AccessIp("0.0.0.0",1L);
+        AccessIp accessIp = new AccessIp("0.0.0.0", 1L);
         HttpServletRequest request = mock(HttpServletRequest.class);
 
         when(accessIpRepository.findById("0.0.0.0")).thenReturn(Optional.of(accessIp));
@@ -54,7 +49,7 @@ class IpBlockManagerTest {
 
     @Test
     public void 마지막_실패_요청으로부터_하루가_지난경우_실패횟수를_초기화() throws Exception {
-        AccessIp accessIp = new AccessIp("0.0.0.0",100L);
+        AccessIp accessIp = new AccessIp("0.0.0.0", 100L);
         accessIp.setUpdatedAt(LocalDateTime.now().minusDays(1L).minusMinutes(5L));
 
         HttpServletRequest request = mock(HttpServletRequest.class);
