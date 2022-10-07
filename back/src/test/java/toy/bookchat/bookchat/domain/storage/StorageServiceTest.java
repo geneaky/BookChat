fc1d8486a025aa9d521a125eab06ef18e0e9af71
geneaky <kaktus418@gmail.com>
2022-10-07 12:20:27 +0900

    [test] actions first

--- back/src/test/java/toy/bookchat/bookchat/domain/storage/StorageServiceTest.java
+++ back/src/test/java/toy/bookchat/bookchat/domain/storage/StorageServiceTest.java
@@ -1,22 +1,16 @@
 package toy.bookchat.bookchat.domain.storage;
 
-import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 import toy.bookchat.bookchat.config.aws.S3Config;
 import toy.bookchat.bookchat.domain.storage.exception.ImageUploadToStorageException;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
