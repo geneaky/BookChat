52fe1de6446e4a28928ac967454a75e5d66ae82d
geneaky <kaktus418@gmail.com>
2022-10-17 18:54:38 +0900

    [chore] revise image size 300 to 200 and update docs

--- src/test/java/toy/bookchat/bookchat/domain/user/api/UserControllerTest.java
+++ src/test/java/toy/bookchat/bookchat/domain/user/api/UserControllerTest.java
@@ -349,7 +349,7 @@ class UserControllerTest extends AuthenticationTestExtension {
                     headerWithName("OIDC").description("Bearer [openid token]")
                 ),
                 requestParts(
-                    partWithName("userProfileImage").description("프로필 이미지 [300 x 300].webp"),
+                    partWithName("userProfileImage").description("프로필 이미지 [200 x 200].webp"),
                     partWithName("userSignUpRequestDto").description("회원가입 입력 폼")
                 ),
                 requestPartFields("userSignUpRequestDto",
