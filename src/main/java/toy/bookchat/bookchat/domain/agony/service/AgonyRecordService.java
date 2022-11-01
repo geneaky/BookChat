1d5042a183b18026053a57054e0dabb626b049ce
geneaky <kaktus418@gmail.com>
2022-11-01 11:12:19 +0900

    [test] serach page of agony record service

--- src/main/java/toy/bookchat/bookchat/domain/agony/service/AgonyRecordService.java
+++ src/main/java/toy/bookchat/bookchat/domain/agony/service/AgonyRecordService.java
@@ -1,12 +1,15 @@
 package toy.bookchat.bookchat.domain.agony.service;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toy.bookchat.bookchat.domain.agony.Agony;
+import toy.bookchat.bookchat.domain.agony.AgonyRecord;
 import toy.bookchat.bookchat.domain.agony.exception.AgonyNotFoundException;
 import toy.bookchat.bookchat.domain.agony.repository.AgonyRecordRepository;
 import toy.bookchat.bookchat.domain.agony.repository.AgonyRepository;
 import toy.bookchat.bookchat.domain.agony.service.dto.CreateAgonyRecordRequestDto;
+import toy.bookchat.bookchat.domain.agony.service.dto.PageOfAgonyRecordsResponse;
 
 @Service
 public class AgonyRecordService {
@@ -29,5 +32,13 @@ public class AgonyRecordService {
                 throw new AgonyNotFoundException("Agony is not registered");
             });
         agonyRecordRepository.save(createAgonyRecordRequestDto.generateAgonyRecord(agony));
+    }
+
+    @Transactional(readOnly = true)
+    public PageOfAgonyRecordsResponse searchPageOfAgonyRecords(Long bookId, Long agonyId,
+        Long userId) {
+        Page<AgonyRecord> agonyRecordPage = agonyRecordRepository.findPageOfUserAgonyRecords(bookId,
+            agonyId, userId);
+        return new PageOfAgonyRecordsResponse(agonyRecordPage);
     }
 }
