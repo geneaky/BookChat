e37165a0bd55200a8d3380697b79630e7386cd67
geneaky <kaktus418@gmail.com>
2022-12-02 08:54:04 +0900

    [test] chat room repository, chat room host repository feat

---
+++ src/test/java/toy/bookchat/bookchat/domain/chatroom/repository/ChatRoomRepositoryTest.java
@@ -0,0 +1,28 @@
+package toy.bookchat.bookchat.domain.chatroom.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import toy.bookchat.bookchat.domain.RepositoryTest;
+import toy.bookchat.bookchat.domain.chatroom.ChatRoom;
+
+@RepositoryTest
+class ChatRoomRepositoryTest {
+
+    @Autowired
+    ChatRoomRepository chatRoomRepository;
+
+    @Test
+    void 채팅방_저장_성공() throws Exception {
+        ChatRoom chatRoom = ChatRoom.builder()
+            .roomName("test room")
+            .roomSize(5)
+            .roomSID("test sid")
+            .build();
+
+        ChatRoom findChatRoom = chatRoomRepository.save(chatRoom);
+
+        assertThat(findChatRoom).isEqualTo(chatRoom);
+    }
+}
