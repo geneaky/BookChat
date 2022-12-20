ce988ba1dd15180972643395dfbe8809c275e974
geneaky <kaktus418@gmail.com>
2022-12-20 18:04:00 +0900

    [backup]

--- src/main/java/toy/bookchat/bookchat/domain/chat/api/ChatController.java
+++ src/main/java/toy/bookchat/bookchat/domain/chat/api/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import toy.bookchat.bookchat.domain.chat.api.dto.ChatDto;
 
 @Controller
 public class ChatController {
@@ -16,6 +17,7 @@ public class ChatController {
 
     @MessageMapping("chat.enter.{chatRoomSid}")
     public void enter(ChatDto chat, @DestinationVariable String chatRoomSid) {
+        
         rabbitTemplate.convertAndSend("amq.topic", "room." + chatRoomSid, chat);
     }
 }
