d809c99b8f0d645b19d00d14fca1ef687c100231
geneaky <kaktus418@gmail.com>
2022-12-21 12:23:34 +0900

    [chore] add websocket local test

--- src/main/java/toy/bookchat/bookchat/domain/chat/api/ChatController.java
+++ src/main/java/toy/bookchat/bookchat/domain/chat/api/ChatController.java
@@ -5,6 +5,8 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import toy.bookchat.bookchat.domain.chat.api.dto.ChatDto;
+import toy.bookchat.bookchat.security.user.TokenPayload;
+import toy.bookchat.bookchat.security.user.UserPayload;
 
 @Controller
 public class ChatController {
@@ -16,8 +18,11 @@ public class ChatController {
     }
 
     @MessageMapping("chat.enter.{chatRoomSid}")
-    public void enter(ChatDto chat, @DestinationVariable String chatRoomSid) {
-        
-        rabbitTemplate.convertAndSend("amq.topic", "room." + chatRoomSid, chat);
+    public void enter(ChatDto chat, @DestinationVariable String chatRoomSid, @UserPayload
+    TokenPayload tokenPayload) {
+        System.out.println(chat);
+        System.out.println(chatRoomSid);
+        System.out.println(tokenPayload);
+        rabbitTemplate.convertAndSend("chat.exchange", "room." + chatRoomSid, chat);
     }
 }
