741422f3bc71368cd10b744aad356dc324642900
geneaky <kaktus418@gmail.com>
2023-02-09 00:22:07 +0900

    [test] chat repository : query user chatroom's chats

---
+++ src/main/java/toy/bookchat/bookchat/domain/chat/repository/query/ChatQueryRepositoryImpl.java
@@ -0,0 +1,42 @@
+package toy.bookchat.bookchat.domain.chat.repository.query;
+
+import static toy.bookchat.bookchat.domain.chat.QChat.chat;
+import static toy.bookchat.bookchat.domain.common.RepositorySupport.extractOrderSpecifierFrom;
+import static toy.bookchat.bookchat.domain.common.RepositorySupport.numberBasedPagination;
+import static toy.bookchat.bookchat.domain.common.RepositorySupport.toSlice;
+import static toy.bookchat.bookchat.domain.participant.QParticipant.participant;
+import static toy.bookchat.bookchat.domain.user.QUser.user;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Repository;
+import toy.bookchat.bookchat.domain.chat.Chat;
+
+@Repository
+public class ChatQueryRepositoryImpl implements ChatQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public ChatQueryRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    @Override
+    public Slice<Chat> getChatRoomChats(Long roomId, Optional<Long> postCursorId, Pageable pageable,
+        Long userId) {
+        return toSlice(queryFactory.select(chat)
+            .from(chat)
+            .join(chat.user, user).fetchJoin()
+            .where(chat.chatRoom.id.eq(JPAExpressions.select(participant.chatRoom.id)
+                    .from(participant)
+                    .where(participant.user.id.eq(userId)
+                        .and(participant.chatRoom.id.eq(roomId)))),
+                numberBasedPagination(chat, chat.id, postCursorId, pageable)
+            )
+            .limit(pageable.getPageSize())
+            .orderBy(extractOrderSpecifierFrom(chat, pageable)).fetch(), pageable);
+    }
+}
