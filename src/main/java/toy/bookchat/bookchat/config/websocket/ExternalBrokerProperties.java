b901961ebf5166041ff61783ab624809547f452c
geneaky <kaktus418@gmail.com>
2023-03-08 17:32:40 +0900

    [hotfix]

--- src/main/java/toy/bookchat/bookchat/config/websocket/ExternalBrokerProperties.java
+++ src/main/java/toy/bookchat/bookchat/config/websocket/ExternalBrokerProperties.java
@@ -12,17 +12,14 @@ public class ExternalBrokerProperties {
     private final String host;
     private final String virtualHost;
     private final int stompPort;
-    private final int amqpPort;
     private final String login;
     private final String passcode;
 
-    public ExternalBrokerProperties(String host, String virtualHost, int stompPort, int amqpPort,
-        String login,
+    public ExternalBrokerProperties(String host, String virtualHost, int stompPort, String login,
         String passcode) {
         this.host = host;
         this.virtualHost = virtualHost;
         this.stompPort = stompPort;
-        this.amqpPort = amqpPort;
         this.login = login;
         this.passcode = passcode;
     }
