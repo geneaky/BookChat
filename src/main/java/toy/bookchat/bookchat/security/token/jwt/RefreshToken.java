b09de15c3aa2b67437d4ee09ef656bb3c98bd78a
geneaky <kaktus418@gmail.com>
2023-04-04 02:09:28 +0900

    [hotfix] rollback columndefinition text no refresh token

--- src/main/java/toy/bookchat/bookchat/security/token/jwt/RefreshToken.java
+++ src/main/java/toy/bookchat/bookchat/security/token/jwt/RefreshToken.java
@@ -1,5 +1,6 @@
 package toy.bookchat.bookchat.security.token.jwt;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class RefreshToken extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
+    @Column(columnDefinition = "TEXT")
     private String refreshToken;
 
     @Builder
