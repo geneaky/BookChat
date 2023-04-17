259bada91dcf61b6b0acdcabf45e88d88fc7745a
geneaky <kaktus418@gmail.com>
2023-04-17 23:45:33 +0900

    [revise&docs] fix spell & remove test log

--- src/main/java/toy/bookchat/bookchat/config/websocket/WebSocketTokenValidationInterceptor.java
+++ src/main/java/toy/bookchat/bookchat/config/websocket/WebSocketTokenValidationInterceptor.java
@@ -6,7 +6,6 @@ import static org.springframework.messaging.simp.stomp.StompCommand.SEND;
 import static org.springframework.messaging.simp.stomp.StompCommand.SUBSCRIBE;
 
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageDeliveryException;
@@ -18,7 +17,6 @@ import toy.bookchat.bookchat.exception.security.DeniedTokenException;
 import toy.bookchat.bookchat.security.token.jwt.JwtTokenManager;
 
 @Component
-@Slf4j
 public class WebSocketTokenValidationInterceptor implements ChannelInterceptor {
 
     private final String BEARER = "Bearer ";
@@ -40,7 +38,6 @@ public class WebSocketTokenValidationInterceptor implements ChannelInterceptor {
                 String token = getJwtTokenFromMessage(accessor);
                 jwtTokenManager.getTokenPayloadFromToken(token);
             } catch (Exception exception) {
-                log.info(exception.getMessage());
                 throw new MessageDeliveryException("Access Denied");
             }
         }
