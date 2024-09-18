876364808f2aa4551af89257e2fc8750eab2fa6d
geneaky <kaktus418@gmail.com>
2024-09-18 22:25:09 +0900

    move: common -> support

---
+++ src/test/java/toy/bookchat/bookchat/support/RateLimiterAopTest.java
@@ -0,0 +1,18 @@
+package toy.bookchat.bookchat.domain.common;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import toy.bookchat.bookchat.support.RateLimit;
+
+class RateLimiterAopTest {
+
+  AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+
+  @Test
+  void pointcut_매칭_성공() throws Exception {
+    pointcut.setExpression("@annotation(toy.bookchat.bookchat.support.RateLimit)");
+    assertThat(pointcut.matches(RateLimit.class)).isTrue();
+  }
+}
